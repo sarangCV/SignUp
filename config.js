c7979d1b9a7815fb5dcef30c98327ea2dd7742ad
sarangcv <sarangcv.jul8@gmail.com>
2020-02-10 10:17:27 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDBuUgXLaECDf9zeq3rnjY0NpUhL_VLSMg",
+    authDomain: "wallpaper-267207.firebaseapp.com",
+    databaseURL: "https://wallpaper-267207.firebaseio.com",
+    projectId: "wallpaper-267207",
+    storageBucket: "wallpaper-267207.appspot.com",
+    messagingSenderId: "85412361352",
+    appId: "1:85412361352:web:1396d6df862e0d62909b3d",
+    measurementId: "G-SNQFMCX5B9"
+  };
